a46239ca28cfb0e0c96bd4acd52037f608f25d15
tthoonee2 <80969262+tthoonee2@users.noreply.github.com>
2021-03-28 15:28:59 +0200

    1.01

---
+++ theory.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+
+
+#to create a new window
+window = tk.Tk()
+#to add a widget
+#greeting = tk.Label(text='hello world')
+#greeting.pack()
+#with window.mainloop() python listens for events
+#window.mainloop() #where mainloop is the method for listening to new events
+#the following are all types of widgets
+#label
+#button
+#entry
+#text
+#frame
+
+#Useful label:
+label = tk.Label(
+    text='Label_txt',
+    fg="font_color", #can be hexadecimal
+    bg='background_color', #can be hexadecimal
+    width='selected witdh',
+)
+
+#getting user imput
+#.get() , .delete() it deletes the text, .insert() inserts the text
+entry = tk.Entry(
+fg = 'yellow',
+bg= 'blue',
+width = 50
+
+)
+#entry get a oneline user input
+
+entry.pack()
+name = entry.get()
+window.mainloop()
+name
+
+#useful button
+button = tk.Button(
+    text='Click me',
+    fg='white',
+    bg='blue'
+
+)
